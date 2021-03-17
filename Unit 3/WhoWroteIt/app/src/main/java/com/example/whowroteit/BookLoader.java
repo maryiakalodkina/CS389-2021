14f855adc14b6f5f7d1f54bf5e7c80f5da0ebb94
Maryia <mk70123n@pace.edu>
2021-03-17 14:19:42 -0400

    Unit 3

---
+++ Unit 3/WhoWroteIt/app/src/main/java/com/example/whowroteit/BookLoader.java
@@ -0,0 +1,30 @@
+package com.example.whowroteit;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader {
+
+    private String mQueryString;
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+}
